dd25eceaba08d41625df1369e9729fe274be4fc7
Fernando196 <fer_jose1996@hotmail.com>
2022-11-19 17:24:55 -0600

    init api blog

---
+++ src/api/index-blog.js
@@ -0,0 +1,48 @@
+require('../config/config');
+const express = require('express');
+const app     = express();
+const http    = require('http');
+const https   = require('https');
+const cors    = require('cors');
+var { expressjwt: jwt } = require('express-jwt');
+const routers = require('./routers');
+
+let unprotected = [
+    '/api/auth',
+    '/api/usuario/registro'
+]
+
+app.use(express.json());
+app.use(cors());
+app.use(
+    jwt({
+        secret: process.env.SEED_API_BLOG,
+        algorithms: ['ES256'],
+        requestProperty: 'userData'
+    }).unless({
+        path: unprotected
+    })
+);
+
+const port = process.env.PORT;
+
+if( process.env.NODE_ENV === 'production' ){
+
+    https
+        .createServer(
+            {
+                key: '',
+                cert: '',
+                ca: ''
+            },
+            app
+        )
+        .listen(port,() => console.log(`Listening at http://localhost:${port}`))
+
+}else{
+
+    http
+        .createServer(app)
+        .listen(port,() => console.log(`Listening at http://localhost:${port}`))
+
+}
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+process.env.SEED_API_BLOG = 'clase-secreta-blog';
+process.env.PORT = 51000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
